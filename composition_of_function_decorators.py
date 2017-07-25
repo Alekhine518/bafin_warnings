5ed2599f37ab1d889ef2df85a253781db437145c
Alekhine518 <caspar.reichenbach@gmail.com>
2017-07-25 10:16:38 +0200

    Create composition_of_function_decorators.py

---
+++ composition_of_function_decorators.py
@@ -0,0 +1,3 @@
+def example_01():
+    def get_text(name):       return "lorem ipsum, {0} dolor sit amet".format(name)
+    def p_decorate(func):       def func_wrapper(name):           return "<p>{0}</p>".format(func(name))       return func_wrapper    my_get_text = p_decorate(get_text)    print(my_get_text("John"))    # The main point seems to be, that the input remains the same, but the    # decorator changes/expands the output.
